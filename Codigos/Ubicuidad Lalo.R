513bcb37b7385681b2aca74ef363dc83d85c995b
SIGEHgo <sigeh@hidalgo.gob.mx>
2026-04-23 11:27:44 -0600

    Avances de lalo sobre ubicuidad

--- Codigos/Ubicuidad Lalo.R
+++ Codigos/Ubicuidad Lalo.R
@@ -17,14 +17,13 @@ datos = datos |>
 interes = c("Area Metropolitana de la Cd. de México", "Pachuca, Hgo.", "Tulancingo, Hgo.")
 
 
-
 transponer = datos |>
   tidyr::pivot_longer(cols = -1, names_to = "Generico") |>
   tidyr::pivot_wider(names_from = 1)
 
 ubicuidad = transponer |>
   dplyr::mutate(
-    Ubicuidad = rowSums(dplyr::across(-1), na.rm = TRUE)
+    Ubicuidad = rowSums(dplyr::across(-1), na.rm = T)
   ) |> 
   dplyr::select(Generico, Ubicuidad) |> 
   dplyr::arrange(Generico)
@@ -54,6 +53,7 @@ ubicuidad_hidalgo = filtro |>
       `Ubicuidad Hidalgo Ciudad` == "1, 0, 1" ~ "Area Metropolitana de la Cd. de México; Tulancingo, Hgo.",
       `Ubicuidad Hidalgo Ciudad` == "1, 1, 0" ~ "Area Metropolitana de la Cd. de México; Pachuca, Hgo.",
       `Ubicuidad Hidalgo Ciudad` == "0, 1, 1" ~ "Pachuca, Hgo.; Tulancingo, Hgo.",
+      T ~ `Ubicuidad Hidalgo Ciudad`
     )
   )
 
@@ -64,4 +64,33 @@ ubicuidad = ubicuidad |>
     y = ubicuidad_hidalgo,
     by = "Generico"
   )
+
+
+ubicuidad |>  openxlsx::write.xlsx("outputs/Ubicuidad.xlsx")
+
+###################################################################
+### Consideramos la media de la ubicuidad, nos quedamos         ###
+### en el caso que esten por debajo de la media y al menos una  ###
+### ciudad del estado de hidalgo.                               ###
+###################################################################
+
+corte = ubicuidad |> 
+  dplyr::filter(Ubicuidad < Ubicuidad |>  mean() |>  floor()) |> 
+  dplyr::arrange(`Ubicuidad Hidalgo` |>  dplyr::desc(), Ubicuidad, Generico) |> 
+  dplyr::filter(`Ubicuidad Hidalgo` != 0)
+
+
+corte |>  openxlsx::write.xlsx("outputs/Ubicuidad_media_corte_hidalgo.xlsx")
+
+##############################################
+### Consideramos todas aquellas que estan  
+### al menos una ciudad de hidalgo
+############################################
+
+una = ubicuidad |> 
+  dplyr::filter(`Ubicuidad Hidalgo` > 0) |> 
+  dplyr::arrange(`Ubicuidad Hidalgo` |>  dplyr::desc(), Ubicuidad, Generico) |> 
+  dplyr::filter(Ubicuidad < Ubicuidad |>  mean() |>  floor())
+
+una |>  openxlsx::write.xlsx("outputs/Ubicuidad_corte_hidalgo_media.xlsx")
 
